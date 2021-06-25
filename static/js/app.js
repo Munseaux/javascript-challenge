2f181a9539c88ad5ba6969d369554ebce7d1d330
Lucas Munson <lucasbmunson@gmail.com>
2021-06-25 14:58:58 -0400

    working solution

--- static/js/app.js
+++ static/js/app.js
@@ -1,44 +1,134 @@
 // from data.js
 var tableData = data;
 
-// YOUR CODE HERE!
-
-var tbody = d3.select("tbody");
-
-tableData.forEach((tableData) => {
-    var row = tbody.append("tr");
-    Object.entries(tableData).forEach(([key, value]) => {
-        var cell = row.append("td");
-        cell.text(value);
+function tableRender(someData) {
+    someData.forEach((someData) => {
+        var row = tbody.append("tr");
+        Object.entries(someData).forEach(([key, value]) => {
+            var cell = row.append("td");
+            cell.text(value);
+        });
     });
-});
+    return true;
+}
 
-var button = d3.select("#filter-btn");
+function makeDropdown(tableData, key){
 
-var form = d3.select("#form");
+    var dropdownList = [];
+    dropdownList.push("--")
+    for(i=0; i<tableData.length; i++){
+        if(dropdownList.includes(tableData[i][key]) === false){
+            dropdownList.push(tableData[i][key]);
+        }
+    }
+    return dropdownList;
+}
 
-button.on("click", runEnter);
-form.on("submit", runEnter);
-
-function runEnter() {
+function filterData() {
 
     d3.event.preventDefault();
+    
+    var dateSelect = d3.select("#date-select");
+    var citySelect = d3.select("#city-select");
+    var stateSelect = d3.select("#state-select");
+    var countrySelect = d3.select("#country-select");
+    var shapeSelect = d3.select("#shape-select");
 
-    var inputElement = d3.select("#datetime");
+    var dateValue = dateSelect.property("value");
+    var cityValue = citySelect.property("value");
+    var stateValue = stateSelect.property("value");
+    var countryValue = countrySelect.property("value");
+    var shapeValue = shapeSelect.property("value");
 
-    var inputValue = inputElement.property("value");
+    
+    var filteredData = tableData;
+    console.log(dateValue)
+    if (dateValue != "--"){
+        filteredData = filteredData.filter(row => row.datetime === dateValue);
+    }
+    if (cityValue !== "--"){
+        filteredData = tableData.filter(row => row.city === cityValue);
+    }
+    if (stateValue !== "--"){
+        filteredData = filteredData.filter(row => row.state === stateValue);
+    }
+    if (countryValue !== "--"){
+        filteredData = filteredData.filter(row => row.country === countryValue);
+    }
+    if(shapeValue !== "--"){
+        filteredData = filteredData.filter(row => row.shape === shapeValue);
+    }
 
-    var filteredData = tableData.filter(row => row.datetime === inputValue);
     console.log(filteredData);
 
     tbody.html("");
 
-    filteredData.forEach((filteredData) => {
-        var row = tbody.append("tr");
-        console.log(filteredData);
-        Object.entries(filteredData).forEach(([key, value]) => {
-            var cell = row.append("td");            
-            cell.text(value);
-        });
-    });
+    tableRender(filteredData);
 }
+
+// from data.js
+
+var tbody = d3.select("tbody");
+
+//render the first unfiltered table
+tableRender(tableData);
+makeDropdown(tableData);
+var dateList = makeDropdown(tableData, "datetime");
+var cityList = makeDropdown(tableData, "city");
+var stateList = makeDropdown(tableData, "state");
+var countryList = makeDropdown(tableData, "country");
+var shapeList = makeDropdown(tableData, "shape");
+
+//get lists of elements to render in the dropdown menus
+
+var defaultOption = ""; 
+d3.select("#date-select")
+    .selectAll('myOptions')
+    .data(dateList)
+    .enter()
+    .append('option')
+    .text(function (d) { return d; }) // text showed in the menu
+    .property("myOptions", function(d){ return d === defaultOption; })
+
+d3.select("#city-select")
+    .selectAll('myOptions')
+    .data(cityList)
+    .enter()
+    .append('option')
+    .text(function (d) { return d; }) // text showed in the menu
+    .attr("value", function (d) { return d; });
+
+d3.select("#state-select")
+    .selectAll('myOptions')
+    .data(stateList)
+    .enter()
+    .append('option')
+    .text(function (d) { return d; }) // text showed in the menu
+    .attr("value", function (d) { return d; });
+
+d3.select("#country-select")
+    .selectAll('myOptions')
+    .data(countryList)
+    .enter()
+    .append('option')
+    .text(function (d) { return d; }) // text showed in the menu
+    .attr("value", function (d) { return d; });
+
+d3.select("#shape-select")
+    .selectAll('myOptions')
+    .data(shapeList)
+    .enter()
+    .append('option')
+    .text(function (d) { return d; }) // text showed in the menu
+    .attr("value", function (d) { return d; });
+
+
+var button = d3.select("#filter-btn");
+
+var form = d3.select("#form");
+    
+button.on("click", filterData);
+form.on("submit", filterData);
+
+
+
